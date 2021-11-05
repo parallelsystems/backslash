83ae7aac3c97a1638c49e505fa8f390d7aa99b7e
Kevin Limburg <kevin@moveparallel.com>
2021-11-05 08:55:49 -0700

    more changes

--- scripts/github_version_fix.py
+++ scripts/github_version_fix.py
@@ -1,12 +1,17 @@
 import os
-import subprocess
 
 if __name__ == "__main__":
-    github_event = os.environ.get('GITHUB_EVENT')
-    if github_event == "tags":
-        version = subprocess.check_output('git describe --tags', shell=True, encoding='utf-8').strip()
+    github_event_name = os.environ.get('GITHUB_EVENT_NAME')
+    github_ref_type = os.environ.get('GITHUB_REF_TYPE')
+    github_sha = os.environ.get("GITHUB_SHA")
+    github_ref = os.environ.get("GITHUB_REF")
+    if github_ref_type == "tag":
+        version = github_ref.replace("refs/tags/", "")
+    elif github_ref_type == "branch":
+        branch = github_ref.replace("refs/heads/", "")
+        version = f"{github_sha}_{branch}"
     else:
-        version = os.environ.get("GITHUB_SHA")
+        version = github_sha
 
     with open('flask_app/__version__.py', 'w') as f:
         print(f'__version__ = "{version}"', file=f)
